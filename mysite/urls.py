85e6d969e5a57c1c3dffabd9cd7c28624f1af1c7
Damian Mosiński <72911883+Straicur@users.noreply.github.com>
2020-11-15 19:40:04 +0100

    Zmiana 21

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    path('accounts/password_change_done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('password/', auth_views.PasswordChangeView.as_view()),
+    path('accounts/password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', include('blog.urls')),
 ]
