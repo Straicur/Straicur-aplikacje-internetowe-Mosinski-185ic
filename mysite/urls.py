9a08d9030053a75c2a3359b38d8d4b65116f3b30
Damian Mosiński <72911883+Straicur@users.noreply.github.com>
2020-11-15 19:43:39 +0100

    Zmiana 21

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('password/', auth_views.PasswordChangeView.as_view()),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html')),
     path('accounts/password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', include('blog.urls')),
 ]
