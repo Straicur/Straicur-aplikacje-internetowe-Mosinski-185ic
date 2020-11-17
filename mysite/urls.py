6249e8c071d75aaf7bb375e0792461cd070360c8
Damian Mosiński <72911883+Straicur@users.noreply.github.com>
2020-11-17 11:14:19 +0100

    Zmiana 22

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path , include
 from django.contrib.auth import views as auth_views
 from blog import views
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', TemplateView.as_view(template_name='post_list.html'), name='post_list'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('signup', views.signup, name='signup'),
     path('', include('blog.urls')),
 ]
