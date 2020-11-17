a86255a534015f9f55f016e2fefc39a4070cf69c
Damian Mosiński <72911883+Straicur@users.noreply.github.com>
2020-11-17 16:11:03 +0100

    Zmiana 23

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path , include
 from django.contrib.auth import views as auth_views
 from blog import views
-from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/password_reset/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/password_reset_done/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('', include('blog.urls')),
     
 ]
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']
