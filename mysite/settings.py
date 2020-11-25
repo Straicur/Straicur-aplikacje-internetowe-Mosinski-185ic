623f31e6f9d88fc9182e0dc4071af9d8aa0c5034
Damian Mosiński <72911883+Straicur@users.noreply.github.com>
2020-11-25 11:10:33 +0100

    Commit43

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,9 +151,4 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mosinskidamian11@gmail.com'
-EMAIL_HOST_PASSWORD = 'umxanqorvoislzoe'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
